c5c1c9155e40fa7015d049da54a9e7df567acf0f
Aidan Tilgner <aidantilgner02@gmail.com>
2021-06-03 00:56:01 -0700

    made the grid background

---
+++ game/src/App.tsx
@@ -0,0 +1,18 @@
+//modules
+import React from 'react';
+
+//components
+import Grid from './Components/Grid/Grid'
+
+//files
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Grid/>
+    </div>
+  );
+}
+
+export default App;
